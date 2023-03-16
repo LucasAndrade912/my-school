fa2395910943f8272bd3a79f8eb00c91074f7898
Lucas Andrade <lucasantos.dev@gmail.com>
2023-03-16 00:53:22 -0300

    fix bugs to create class

--- server/src/useCases/Class/CreateClassUseCase.ts
+++ server/src/useCases/Class/CreateClassUseCase.ts
@@ -14,7 +14,7 @@ export class CreateClassUseCase {
     private validator: ValidatorAdapter
 	) {}
 
-	public async execute({ courseId, data }: CreateClassUseCaseRequest) {
+	async execute({ courseId, data }: CreateClassUseCaseRequest) {
 		const schema = this.validator.createSchema({
 			name: this.validator.string(),
 			description: this.validator.string({ nullable: true }),
@@ -25,58 +25,64 @@ export class CreateClassUseCase {
 
 		schema.validate(data)
 
-		if (data.endTime < data.startTime) {
-			throw new Error('End time cannot be less than the start time', { cause: 'Invalid class duration time' })
-		}
-
-		const classTimeIsLessThan15Minutes = !((data.endTime - data.startTime) >= 15)
-
-		if (classTimeIsLessThan15Minutes) {
-			throw new Error('Duration of the lesson should be at least 15 minutes', { cause: 'Very short class time' })
-		}
-
-		for (const weekDay of data.weekDays) {
-			const dayHasPassed = verifyIfWeekDayHasPassed(weekDay)
-
-			if (dayHasPassed) {
-				throw new Error('You cannot create classes on a day of the week that has passed', { cause: 'Day has passed' })
-			}
-		}
-
 		try {
-			const [classes] = await Promise.all(
+			if (data.endTime < data.startTime) {
+				throw new Error('End time cannot be less than the start time', { cause: 'Invalid class duration time' })
+			}
+
+			const classTimeIsLessThan15Minutes = !((data.endTime - data.startTime) >= 15)
+
+			if (classTimeIsLessThan15Minutes) {
+				throw new Error('Duration of the lesson should be at least 15 minutes', { cause: 'Very short class time' })
+			}
+
+			for (const weekDay of data.weekDays) {
+				const dayHasPassed = verifyIfWeekDayHasPassed(weekDay)
+
+				if (dayHasPassed) {
+					throw new Error('You cannot create classes on a day of the week that has passed', { cause: 'Day has passed' })
+				}
+			}
+
+			const weekDayClasses = await Promise.all(
 				data.weekDays.map(weekDay => this.repository.findClassesByWeekDay(courseId, weekDay))
 			)
 
-			for (const clas of classes) {
-				const startTimeOfNewClassIsBetweenDurationTimeOfOtherClass = (
-					data.startTime >= clas.startTime &&
-          data.startTime < clas.endTime
-				)
+			for (const classes of weekDayClasses) {
+				for (const clas of classes) {
+					const startTimeOfNewClassIsBetweenDurationTimeOfOtherClass = (
+						data.startTime >= clas.startTime &&
+            data.startTime < clas.endTime
+					)
 
-				const endTimeOfNewClassIsBetweenDurationTimeOfOtherClass = (
-					data.endTime > clas.startTime &&
-          data.endTime < clas.endTime
-				)
+					const endTimeOfNewClassIsBetweenDurationTimeOfOtherClass = (
+						data.endTime > clas.startTime &&
+            data.endTime < clas.endTime
+					)
 
-				const classTimeConflictsWithAnotherClass = (
-					(clas.endTime - clas.startTime) < (data.endTime - data.startTime)
-				)
+					const classTimeConflictsWithAnotherClass = (
+						(clas.endTime - clas.startTime) < (data.endTime - data.startTime)
+					)
 
-				const happensDuringTheTimeOfAnotherClass = (
-					startTimeOfNewClassIsBetweenDurationTimeOfOtherClass ||
-          endTimeOfNewClassIsBetweenDurationTimeOfOtherClass
-				) &&
-        (classTimeConflictsWithAnotherClass)
+					const happensDuringTheTimeOfAnotherClass = (
+						startTimeOfNewClassIsBetweenDurationTimeOfOtherClass ||
+            endTimeOfNewClassIsBetweenDurationTimeOfOtherClass ||
+            classTimeConflictsWithAnotherClass
+					)
 
-				if (happensDuringTheTimeOfAnotherClass) {
-					throw new Error('One class cannot be created at the same time as another')
+					if (happensDuringTheTimeOfAnotherClass) {
+						throw new Error('One class cannot be created at the same time as another', {
+							cause: 'Class duration schedule is conflicting with another'
+						})
+					}
 				}
 			}
 
 			await this.repository.insert(courseId, { ...data, assisted: false })
 		} catch (err) {
-			return err
+			if (err instanceof Error) console.log('Cause ->', err.cause)
+
+			throw err
 		}
 	}
 }
