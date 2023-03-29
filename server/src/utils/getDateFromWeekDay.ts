e3bba0a5b552d7cac9ffd1c927e5db33ad492fd7
Lucas Andrade <lucasantos.dev@gmail.com>
2023-03-28 23:43:40 -0300

    fix instruction to remove timezone from a date

--- server/src/utils/getDateFromWeekDay.ts
+++ server/src/utils/getDateFromWeekDay.ts
@@ -2,13 +2,13 @@ import { formatDateToISOString } from './formatDateToISOString'
 
 export function getDateFromWeekDay(weekDay: number) {
 	const today = new Date()
-	const timeZone = today.getTimezoneOffset() / 60
-	today.setHours(today.getHours() - timeZone)
-
 	const additionalDay = weekDay - today.getDay()
 
 	const date = new Date(today)
 	date.setDate(new Date().getDate() + additionalDay)
+
+	const timeZone = date.getTimezoneOffset() / 60
+	date.setHours(date.getHours() - timeZone)
 
 	return formatDateToISOString(date)
 }
